3930f1717fda6a3b363ce2fdf1425992792e0070
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-10-06 11:27:08 -0300

    added interface

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    // response.data has properties of:
+    // id
+    // title
+    // completed
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log(id + "," + title + "," + completed);
 });
--- index.ts
+++ index.ts
@@ -2,6 +2,20 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1"
 
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 axios.get(url).then(response => {
-    console.log(response.data);
+    // response.data has properties of:
+    // id
+    // title
+    // completed
+    const todo = response.data as Todo;
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+    console.log(`${id},${title},${completed}`);
 })
